5685e0649d3bcf9ce359fef833a8da5fe5b1f48e
Chris Radcliffe <chris.a.radcliffe@outlook.com>
2026-03-08 22:56:12 +0300

    removed testing lines in main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import shutil
 import pathlib
 
 if __name__ == "__main__":
-    wef = pathlib.Path(r"C:\Users\chris\AppData\Local\Microsoft\Office\16.0\Wef")
-    if wef.exists():
-        for item in wef.iterdir():
-            shutil.rmtree(item) if item.is_dir() else item.unlink()
-        print(f"Cleared Office cache: {wef}")
+    
+    ## helper function for testing. Clears the Microsoft Office cache for development updates. 
+    # wef = pathlib.Path(r"C:\Users\chris\AppData\Local\Microsoft\Office\16.0\Wef")
+    # if wef.exists():
+    #     for item in wef.iterdir():
+    #         shutil.rmtree(item) if item.is_dir() else item.unlink()
+    #     print(f"Cleared Office cache: {wef}")
 
     uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=False)
